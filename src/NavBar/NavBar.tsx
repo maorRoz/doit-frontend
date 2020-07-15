91ae641ff4fd868f0f8f993d17d89ee5109e210d
Maor Rozevich <andorys@post.bgu.ac.il>
2020-07-16 02:18:16 +0300

    removed redundant popper import

--- src/NavBar/NavBar.tsx
+++ src/NavBar/NavBar.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Popper } from '@material-ui/core';
 import { NavBarLayout, NavBarHeader } from './style';
 import { MenuItem } from './types';
 import { NavBarItem } from './NavBarItem';
