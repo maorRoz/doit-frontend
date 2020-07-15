1aa3d4930103f980a3206d0ec979fecfe6d80e25
Maor Rozevich <andorys@post.bgu.ac.il>
2020-07-16 02:16:05 +0300

    added menus.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,7 @@
 import React from 'react';
 import { NavBar } from './NavBar/NavBar';
+import items from './menus.json';
 
-export const App = () => (
-  <NavBar
-    items={[
-      {
-        name: 'Hello',
-        subItems: [
-          {
-            name: 'child',
-            subItems: [
-              { name: 'child child', subItems: [{ name: 'child child child' }] }
-            ]
-          },
-          { name: 'child2' }
-        ]
-      },
-      { name: 'World1111111', subItems: [{ name: 'child2' }] },
-      { name: 'Wtf' }
-    ]}
-  />
-);
+export const App = () => <NavBar items={items} />;
 
 export default App;
